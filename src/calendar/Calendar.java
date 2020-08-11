6dbc23082124e6a520798fa994f25e794d97100d
cheon <cheon@cheon-PC>
2020-08-11 17:42:43 +0900

    Init

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,7 +4,7 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hell, Calendar");
+		System.out.println("Hello, Calendar");
 	}
 
 }
